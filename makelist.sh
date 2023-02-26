acff010c53239042ca3004589739af1d7dfea810
Naoya Hayashi <fuzmare@pm.me>
2023-02-27 02:07:12 +0900

    first commit

---
+++ makelist.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+json=$(curl https://fdroid.gitlab.io/mirror-monitor/report.json)
+timestamps=$(echo "($(echo $json | grep -o '"https://f-droid.org/repo": {[^\}]* "timestamp": [0-9]*' | grep -o '"timestamp": [0-9]*' | grep -o '[0-9]*' | tail -30 | tr "\n" "|" | sed -e "s/|$//"))")
+res=$(echo $json | grep -Eo "\"https://[^\"]*\": {[^\}]* \"fingerprint\": \"43238D512C1E5EB2D6569F4A3AFBF5523418B82E0A3ED1552770ABB9A9C9CCAB\", [^\}]* \"timestamp\": $timestamps" | grep -o "^\"https://[^\"]*" | tr -d '"' | sort | uniq -c | sort -n | awk '{if ($1>=29) print $2}' | sort)
+echo "$res" > fdroidmirrors
+rm -rf QR
+mkdir QR
+for mirror in $res
+do
+  name=$(echo $mirror | grep -o 'https://[^/]*' | sed -e "s%https://%%")
+  qrencode -t UTF8 "${mirror}?fingerprint=43238D512C1E5EB2D6569F4A3AFBF5523418B82E0A3ED1552770ABB9A9C9CCAB" -o QR/$name.txt
+  qrencode -t PNG "${mirror}?fingerprint=43238D512C1E5EB2D6569F4A3AFBF5523418B82E0A3ED1552770ABB9A9C9CCAB" -o QR/$name.png
+done
+
